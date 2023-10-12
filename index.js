fc0dc3ef4c1ee8490ff342e17874c84006aead36
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-10-12 14:48:21 +0300

    Updated code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+main.remove()
+let h1=document.createElement("h1");
+h1.id="victory";
+h1.textContent="newHeader";
+h1.innerHTML ="Shalom is the champion";
+
+
+ 
